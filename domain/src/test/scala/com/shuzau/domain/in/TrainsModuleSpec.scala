e0adcc4ec388bc39f8edc584203b70f172ef16e9
shuzau <siarhei.huzau@grandparade.co.uk>
2021-05-22 23:40:39 +0200

    Initial TrainsModule

--- domain/src/test/scala/com/shuzau/domain/in/TrainsModuleSpec.scala
+++ domain/src/test/scala/com/shuzau/domain/in/TrainsModuleSpec.scala
@@ -17,12 +17,27 @@ class TrainsModuleSpec extends AnyFlatSpec with Matchers with ScalaCheckProperty
     unsafeRun(TrainsModule.report()) shouldBe empty
   }
 
-  it should "return not register trip for empty storage" in {
+  it should "not register trip for empty storage" in {
     forAll(EntityGen.trip) { trip =>
       unsafeRun(TrainsModule.register(trip).either) shouldBe Symbol("left")
     }
   }
 
+  it should "register trip with known train and stations" in {
+    forAll(for {
+      version  <- EntityGen.version
+      train    <- EntityGen.train(version)
+      stations <- EntityGen.stations(version)
+      trip     <- EntityGen.trip(version, train, stations)
+    } yield (train, stations, trip)) { case (train, stations, trip) =>
+      val scenario = for {
+        _ <- TrainsModule.register(train)
+        _ <- ZIO.foreach_(stations)(TrainsModule.register)
+        _ <- TrainsModule.register(trip)
+      } yield ()
+      unsafeRun(scenario.either) shouldBe Symbol("right")
+    }
+  }
   def unsafeRun[T](scenario: ZIO[ZEnv with TrainsService, Nothing, T]): T =
     default.unsafeRun(scenario.provideCustomLayer(TrainsModule.layer))
 }
